e053b67636111a232e548512a8ad8cc6730eeff3
fine <tianrandailoving@gmail.com>
2019-07-17 21:52:37 +0800

    init project and add dependency

---
+++ Pods/Target Support Files/Pods-QRMaker/Pods-QRMaker-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_QRMaker : NSObject
+@end
+@implementation PodsDummy_Pods_QRMaker
+@end
---
+++ QRMaker/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  QRMaker
+//
+//  Created by finefine on 2019/7/17.
+//  Copyright © 2019 finefine. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
