b2ed5afb196731ca8d79ec0f9fd1f9348c7b8b9a
fine <tianrandailoving@gmail.com>
2019-07-17 22:18:55 +0800

    implement the QR generate UI and logic

--- QRMaker/ViewController.swift
+++ QRMaker/ViewController.swift
@@ -7,9 +7,12 @@
 //
 
 import Cocoa
+import EFQRCode
 
 class ViewController: NSViewController {
 
+    @IBOutlet weak var inputText: NSTextField!
+    @IBOutlet weak var outputImage: NSImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -22,6 +25,24 @@ class ViewController: NSViewController {
         }
     }
 
-
+    @IBAction func buildQR(_ sender: NSButton) {
+        if let image = generateQR(text: inputText.stringValue){
+            outputImage.image = NSImage(cgImage: image, size: outputImage.frame.size)
+        }
+    }
+    
+    @IBAction func saveTo(_ sender: NSButton) {
+    }
+    
+    private func generateQR(text:String) -> CGImage? {
+        
+        if let tryImage = EFQRCode.generate(content: text){
+            print("Create QRCode success:\(tryImage)")
+            return tryImage
+        }else{
+            print("Create QRCode failed")
+            return nil
+        }
+    }
 }
 
